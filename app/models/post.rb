8dd0c8c3bf42b6d80cefec295397a853bef64ca6
David Kennell <davidkennell0@gmail.com>
2017-01-02 11:58:08 -0500

    Done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,21 +1,24 @@
 class Post < ActiveRecord::Base
 
-
-  before_validation do
-    puts "CURRENT CALLBACK: before_validation"
-  end
-
-  after_validation do
-    puts "CURRENT CALLBACK: after_validation"
-  end
-
-  before_save do
-    puts "CURRENT CALLBACK: before_save"
-  end
   #
-  # around_save do
-  #   puts "CURRENT CALLBACK: around_save"
+  # before_validation do
+  #   puts "CURRENT CALLBACK: before_validation"
   # end
+  #
+  # after_validation do
+  #   puts "CURRENT CALLBACK: after_validation"
+  # end
+  #
+  # before_save do
+  #   puts "CURRENT CALLBACK: before_save"
+  # end
+
+  around_save do
+    puts "CURRENT CALLBACK: around_save(before yield)"
+    yield 
+    puts "CURRENT CALLBACK: around_save(after yield)"
+  end
+
   #
   # before_create do
   #   puts "CURRENT CALLBACK: before_create"
@@ -29,12 +32,12 @@ class Post < ActiveRecord::Base
   #   puts "CURRENT CALLBACK: after_create"
   # end
 
-  after_save do
-    puts "CURRENT CALLBACK: after_save"
-  end
-
-  after_commit do
-    puts "CURRENT CALLBACK: after_commit"
-  end
-
+  # after_save do
+  #   puts "CURRENT CALLBACK: after_save"
+  # end
+  #
+  # after_commit do
+  #   puts "CURRENT CALLBACK: after_commit"
+  # end
+  #
 end
