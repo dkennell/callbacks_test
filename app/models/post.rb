2c74b7caa89cabe9f08de497cf18d7f1da079467
David Kennell <davidkennell0@gmail.com>
2017-01-04 13:47:39 -0500

    Finished for now

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,41 +1,45 @@
 class Post < ActiveRecord::Base
 
-  #
-  # before_validation do
-  #   puts "CURRENT CALLBACK: before_validation"
-  # end
-  #
-  # after_validation do
-  #   puts "CURRENT CALLBACK: after_validation"
-  # end
-  #
-  # before_save do
-  #   puts "CURRENT CALLBACK: before_save"
-  # end
 
-  around_save do
-    puts "CURRENT CALLBACK: around_save(before yield)"
-    yield 
-    puts "CURRENT CALLBACK: around_save(after yield)"
+  before_validation do
+    puts "CURRENT CALLBACK: before_validation"
   end
 
-  #
-  # before_create do
-  #   puts "CURRENT CALLBACK: before_create"
-  # end
-  #
-  # around_create do
-  #   puts "CURRENT CALLBACK: around_create"
-  # end
-  #
-  # after_create do
-  #   puts "CURRENT CALLBACK: after_create"
-  # end
+  after_validation do
+    puts "CURRENT CALLBACK: after_validation"
+  end
 
-  # after_save do
-  #   puts "CURRENT CALLBACK: after_save"
-  # end
-  #
+  before_save do
+    puts "CURRENT CALLBACK: before_save"
+  end
+
+  around_save do |a, b|
+      puts "CURRENT CALLBACK: around_save(before yield)"
+      # puts "Record/'Activity': #{a}"
+      # puts "Block: #{b}"
+      b.call
+      puts "CURRENT CALLBACK: around_save(after yield)"
+    end
+
+
+  before_create do
+    puts "CURRENT CALLBACK: before_create"
+  end
+
+  around_create do |a, b|
+    puts "CURRENT CALLBACK: around_create(before yield)"
+    b.call
+    puts "CURRENT CALLBACK: around_create(after yield)"
+  end
+
+  after_create do
+    puts "CURRENT CALLBACK: after_create"
+  end
+
+  after_save do
+    puts "CURRENT CALLBACK: after_save"
+  end
+
   # after_commit do
   #   puts "CURRENT CALLBACK: after_commit"
   # end
